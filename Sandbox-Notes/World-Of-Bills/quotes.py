9bd99adcdb70b57b6769389111610833ba6b7721
Colton <48069930+Mosier44@users.noreply.github.com>
2022-03-04 19:25:56 -0600

    Create quotes.py
    Basic skeleton for reading through the quotes.csv and writing all quotes and topics to their specific directory.

---
+++ Sandbox-Notes/World-Of-Bills/quotes.py
@@ -0,0 +1,23 @@
+csvFile = open('quotes.csv', 'r')
+
+Lines = csvFile.readlines()
+
+for line in Lines:
+    quote, aurthor, topic = line.split(';')
+    if aurthor.split()[0].lower == 'user1firstname':
+        f1 = open('/home/user1/quotes/smile.txt', 'at')
+        f2 = open('/home/user1/quotes/topics.txt', 'at')
+        f1.write(line)
+        f2.write(topic)
+        f2.write('\n')
+    elif aurthor.split()[0].lower() == 'user2firstname':
+        f1 = open('/home/user2/quotes/smile.txt', 'at')
+        f2 = open('/home/user2/quotes/topics.txt', 'at')
+        f1.write(line)
+        f2.write(topic)
+        f2.write('\n')
+ '''
+ Continue creating elif statements until all users are accounted for.
+ '''
+    else:
+        continue
